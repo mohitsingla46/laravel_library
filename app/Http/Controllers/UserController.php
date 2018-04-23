32a6659e5a2d98bf0b3b512c6bf5d71189724dce
= <=>
2018-04-23 17:09:22 +0530

    updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function dashboard(){
-    	return view('dashboard');
+    public function dashboard(Request $request){
+    	$user_id = $request->session()->get('user_id');
+    	$user = DB::table('users')->where('ID','=',$user_id)->first();
+    	if($user){
+    		echo $user->name;
+    	}
+    	else{
+    		echo "error";
+    	}
+    	//return view('dashboard');
     }
 }
